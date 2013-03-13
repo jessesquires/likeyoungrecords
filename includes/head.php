7708b95322e8f0efd9bab5ba6fbf912f0d3cc341
Jesse Squires <jesse.d.squires@gmail.com>
2013-03-13 14:20:01 -0400

    meta tags, about p

--- includes/head.php
+++ includes/head.php
@@ -1,8 +1,8 @@
   	
   	<meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="keywords" content="">
-    <meta name="description" content="">
+    <meta name="keywords" content="like young, record label, records, tapes, vinyl, indie records, independent record label, nosie, ambient, minimal, indie, pop">
+    <meta name="description" content="NOSIE / AMBIENT / MINIMAL / INDIE / POP GALORE!">
     <meta name="author" content="Like Young Records">
 
     <!-- styles -->
--- index.php
+++ index.php
@@ -8,7 +8,10 @@
   <body>
     <?php include('includes/header.php'); ?>
 
-    <p>Like Young Records is a small, independent label that is based in New Albany, IN and run by Jordan Reidinger. NOSIE / AMBIENT / MINIMAL / INDIE / POP. Like Young Records is a small, independent label that is based in New Albany, IN and run by Jordan Reidinger. NOSIE / AMBIENT / MINIMAL / INDIE / POP. Like Young Records is a small, independent label that is based in New Albany, IN and run by Jordan Reidinger. NOSIE / AMBIENT / MINIMAL / INDIE / POP. Like Young Records is a small, independent label that is based in New Albany, IN and run by Jordan Reidinger. NOSIE / AMBIENT / MINIMAL / INDIE / POP. Like Young Records is a small, independent label that is based in New Albany, IN and run by Jordan Reidinger. NOSIE / AMBIENT / MINIMAL / INDIE / POP. </p>
+    <div id="about">
+      <p>Like Young Records is a small, independent record label that is based in <a href="http://maps.google.com/maps/place?ftid=&q=New+Albany" target="_blank">New Albany, IN</a> (seriously, <em>that</em> place) and is run by <a href="http://www.facebook.com/jordan.reidinger" target="_blank">Jordan Reidinger</a>. Like Young primarily releases cassettes full of nosie / ambient / minimal / indie / pop galore! We are dedicated to spreading good music and helping bands as much as possible. Fuck money. Mind the tape.</p>
+      <p class="text-center">Tonight, we will live like young.</p>
+    </div>
     <hr>
     <div class="center" id="fb-container"></div>
     
