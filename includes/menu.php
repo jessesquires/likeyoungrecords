7746945fe82962d0c011b79efbe02407454ec800
Jesse Squires <jesse.d.squires@gmail.com>
2013-03-25 01:49:16 -0400

    menu

--- includes/menu.php
+++ includes/menu.php
@@ -1,7 +1,7 @@
 <ul class="nav nav-pills">
 	<li <?php if ($thisPage == "news") echo "class=\"active\""; ?> ><a href="/">NEWS</a></li>
 	<li class="dropdown <?php if ($thisPage == "releases") echo "active"; ?>">
-    	<a class="dropdown-toggle" data-toggle="dropdown">RELEASES<b class="caret"></b></a>
+    	<a class="dropdown-toggle" data-toggle="dropdown" href="#">RELEASES<b class="caret"></b></a>
 		<ul class="dropdown-menu">
 			<li><a href="releases.php">Releases</a></li>
 			<li><a href="digitalseries.php">Digital Series</a></li>
