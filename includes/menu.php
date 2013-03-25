ee3614e8aa7e6f22472d21c1f2fdc278147484d4
Jesse Squires <jesse.d.squires@gmail.com>
2013-03-25 12:51:06 -0400

    htaccess

--- includes/menu.php
+++ includes/menu.php
@@ -3,10 +3,10 @@
 	<li class="dropdown <?php if ($thisPage == "releases") echo "active"; ?>">
     	<a class="dropdown-toggle" data-toggle="dropdown" href="#">RELEASES<b class="caret"></b></a>
 		<ul class="dropdown-menu">
-			<li><a href="releases.php">Releases</a></li>
-			<li><a href="digitalseries.php">Digital Series</a></li>
+			<li><a href="releases">Releases</a></li>
+			<li><a href="digitalseries">Digital Series</a></li>
       	</ul>
     </li>
-	<li <?php if ($thisPage == "contact") echo "class=\"active\""; ?> ><a href="contact.php">CONTACT</a></li>
+	<li <?php if ($thisPage == "contact") echo "class=\"active\""; ?> ><a href="contact">CONTACT</a></li>
 	<li><a href="http://likexyoung.bigcartel.com">STORE</a></li>
 </ul> <!-- pills -->
