1345221905473ff133c45b3dd68d61e0261d1706
Jesse Squires <jesse.d.squires@gmail.com>
2013-03-31 19:33:50 -0400

    grid debug

--- bootstrap-photo-grid/imageList.php
+++ bootstrap-photo-grid/imageList.php
@@ -1,24 +1,23 @@
 <?php
 $fileList = array();
 $dir = opendir('../img/releases/');
-$index = 0;
 
 while($file = readdir($dir)) {
 	if($file == '.' || $file == '..' || $file == '.DS_Store') {
         continue;
     }
-    $fileList[$index] = $file;
-    $index++;
-    // array_push($fileList, $file);
+    array_push($fileList, $file);
 }
 
-$images = array();
+$imageList = array();
 for($i = 0; $i < count($fileList) - 1; $i+=2) {
 	$j = $i + 1;
 	$imageRecord = array('full' => $fileList[$i],
 						 'thumb' => $fileList[$j]);
-	array_push($images, $imageRecord);
+	array_push($imageList, $imageRecord);
 }
 
-echo json_encode($images);
+asort($imageList);
+
+echo json_encode($imageList);
 ?>
