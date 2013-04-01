c4ae3be8761fd02bfb4cf1a28f5aeffa3f132b2f
Jesse Squires <jesse.d.squires@gmail.com>
2013-03-31 20:12:47 -0400

    grid

--- bootstrap-photo-grid/imageList.php
+++ bootstrap-photo-grid/imageList.php
@@ -17,16 +17,5 @@ for($i = 0; $i < count($fileList) - 1; $i+=2) {
 	array_push($imageList, $imageRecord);
 }
 
-asort($imageList);
-
-$images = array();
-foreach($imageList as $key=>$val) {
-	$f = $val['full'];
-	$t = $val['thumb']; 
-	//echo "Key = $key, Value = $f : $t <br />";
-
-	array_push($images, $val);
-}
-//echo "<br /><br />";
-echo json_encode($images);
+echo json_encode($imageList);
 ?>
