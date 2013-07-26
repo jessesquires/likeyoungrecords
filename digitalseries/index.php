656b9f09a4d39543aafe1bd522ed6e4252d06d91
Jesse Squires <jesse.d.squires@gmail.com>
2013-07-26 13:17:16 -0400

    LYD005 this new moon

--- digitalseries/index.php
+++ digitalseries/index.php
@@ -1,26 +1,27 @@
 <!DOCTYPE html>
 <?php $thisPage="releases"; ?>
 <html lang="en">
-  <head>
-    <title>LYR | Digital Series</title>
-    <?php include('../includes/head.php'); ?>
-  </head>
-  <body>
-    <?php include('../includes/banner.php'); ?>
-    <?php include('../includes/menu.php'); ?>
-    
-    <div class="row">
-      <div id="digitalseries" class="span9">
-  		<h3>Digital Series</h3>
-  		<ul>
-  			<li>LYD001: Like Young - <a href="http://likexyoung.bandcamp.com/album/fame" target="_blank">Fame</a></li>
-  			<li>LYD002: Perfume - <a href="http://likexyoung.bandcamp.com/album/perfume" target="_blank">Perfume</a></li>
-  			<li>LYD003: SEXES - <a href="http://likexyoung.bandcamp.com/album/humans" target="_blank">Humans</a></li>
-  			<li>LYD004: SEXES - <a href="http://likexyoung.bandcamp.com/album/iv" target="_blank">IV</a></li>
-  		</ul>
-      </div> <!-- digitalseries -->
-    </div> <!-- row -->
+	<head>
+		<title>LYR | Digital Series</title>
+		<?php include('../includes/head.php'); ?>
+	</head>
+	<body>
+		<?php include('../includes/banner.php'); ?>
+		<?php include('../includes/menu.php'); ?>
+		
+		<div class="row">
+			<div id="digitalseries" class="span9">
+			<h3>Digital Series</h3><br />
+			<ul>
+				<li>LYD005: Like Young - <a href="http://likexyoung.bandcamp.com/album/this-new-moon-3" target="_blank">This New Moon</a></li>
+				<li>LYD004: SEXES - <a href="http://likexyoung.bandcamp.com/album/iv" target="_blank">IV</a></li>
+				<li>LYD003: SEXES - <a href="http://likexyoung.bandcamp.com/album/humans" target="_blank">Humans</a></li>
+				<li>LYD002: Perfume - <a href="http://likexyoung.bandcamp.com/album/perfume" target="_blank">Perfume</a></li>
+				<li>LYD001: Like Young - <a href="http://likexyoung.bandcamp.com/album/fame" target="_blank">Fame</a></li>
+			</ul>
+			</div> <!-- digitalseries -->
+		</div> <!-- row -->
 
-    <?php include('../includes/footer.php'); ?>
-  </body>
+		<?php include('../includes/footer.php'); ?>
+	</body>
 </html>
