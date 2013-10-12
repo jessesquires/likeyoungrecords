e09e640d51f5c9b3d3edd4494afe03ac9a1903d3
Jesse Squires <jesse.d.squires@gmail.com>
2013-10-11 22:22:34 -0700

    update digital series. daze ghoulish.

--- digitalseries/index.php
+++ digitalseries/index.php
@@ -13,11 +13,13 @@
 			<div id="digitalseries" class="span9">
 			<h3>Digital Series</h3><br />
 			<ul>
-				<li>LYD005: Like Young - <a href="http://likexyoung.bandcamp.com/album/this-new-moon-3" target="_blank">This New Moon</a></li>
-				<li>LYD004: SEXES - <a href="http://likexyoung.bandcamp.com/album/iv" target="_blank">IV</a></li>
-				<li>LYD003: SEXES - <a href="http://likexyoung.bandcamp.com/album/humans" target="_blank">Humans</a></li>
-				<li>LYD002: Perfume - <a href="http://likexyoung.bandcamp.com/album/perfume" target="_blank">Perfume</a></li>
-				<li>LYD001: Like Young - <a href="http://likexyoung.bandcamp.com/album/fame" target="_blank">Fame</a></li>
+				<li>LYD007: Daze Ghoulish / <a href="http://likexyoung.bandcamp.com/album/news" target="_blank">News</a></li>
+				<li>LYD006: Daze Ghoulish / <a href="http://likexyoung.bandcamp.com/album/lovers-hunt-2" target="_blank">Lovers Hunt</a></li>
+				<li>LYD005: Like Young / <a href="http://likexyoung.bandcamp.com/album/this-new-moon-3" target="_blank">This New Moon</a></li>
+				<li>LYD004: SEXES / <a href="http://likexyoung.bandcamp.com/album/iv" target="_blank">IV</a></li>
+				<li>LYD003: SEXES / <a href="http://likexyoung.bandcamp.com/album/humans" target="_blank">Humans</a></li>
+				<li>LYD002: Perfume / <a href="http://likexyoung.bandcamp.com/album/perfume" target="_blank">Perfume</a></li>
+				<li>LYD001: Like Young / <a href="http://likexyoung.bandcamp.com/album/fame" target="_blank">Fame</a></li>
 			</ul>
 			</div> <!-- digitalseries -->
 		</div> <!-- row -->
