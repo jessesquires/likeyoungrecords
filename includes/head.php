949687cf352562faa6c05b7b5223f2baa959d496
Jesse Squires <jesse.d.squires@gmail.com>
2013-11-02 11:52:47 -0700

    bootstrap 3.0.1

--- includes/head.php
+++ includes/head.php
@@ -9,10 +9,11 @@
 		<link href="/bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css" media="screen">
 		<link href="/css/style.css" rel="stylesheet" type="text/css">
 
-		<!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
+		<!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+		<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
 		<!--[if lt IE 9]>
-			<script src="/bootstrap/js/html5shiv.js"></script>
-			<script src="/bootstrap/js/respond.min.js"></script>
+			<script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js" type="text/javascript"></script>
+			<script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js" type="text/javascript"></script>
 		<![endif]-->
 
 		<!-- fav and touch icons -->
