c254f40e62d3d303a89d9766931d144c680a1fad
Jesse Squires <jesse.d.squires@gmail.com>
2013-12-30 22:57:04 -0800

    touch icons + optimize images

--- includes/head.php
+++ includes/head.php
@@ -16,11 +16,17 @@
 			<script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js" type="text/javascript"></script>
 		<![endif]-->
 
-		<!-- fav and touch icons -->
-		<link rel="apple-touch-icon-precomposed" sizes="144x144" href="/ico/apple-touch-icon-144-precomposed.png">
-		<link rel="apple-touch-icon-precomposed" sizes="114x114" href="/ico/apple-touch-icon-114-precomposed.png">
-		<link rel="apple-touch-icon-precomposed" sizes="72x72" href="/ico/apple-touch-icon-72-precomposed.png">
-		<link rel="apple-touch-icon-precomposed" href="/ico/apple-touch-icon-57-precomposed.png">
+		<!-- touch icons iOS 6 -->
+		<link rel="apple-touch-icon-precomposed" sizes="57x57" href="/ico/icon57.png">
+		<link rel="apple-touch-icon-precomposed" sizes="72x72" href="/ico/icon72.png">
+		<link rel="apple-touch-icon-precomposed" sizes="114x114" href="/ico/icon114.png">
+		<link rel="apple-touch-icon-precomposed" sizes="144x144" href="/ico/icon144.png">
+
+		<!-- touch icons iOS 7 -->
+		<link rel="apple-touch-icon" sizes="76x76" href="/ico/icon76.png">
+		<link rel="apple-touch-icon" sizes="120x120" href="/ico/icon120.png">
+		<link rel="apple-touch-icon" sizes="152x152" href="/ico/icon152.png">
+		
 		<link rel="shortcut icon" href="/favicon.ico">
 		
 		<script type="text/javascript">
