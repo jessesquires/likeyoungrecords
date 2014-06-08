e160b983a78bc14aa78fdc0ff2744f6570f83889
Jesse Squires <jesse.d.squires@gmail.com>
2014-06-07 17:00:54 -0700

    update bootstrap template

--- includes/head.php
+++ includes/head.php
@@ -16,7 +16,7 @@
 		<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
 		<!--[if lt IE 9]>
 			<script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js" type="text/javascript"></script>
-			<script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js" type="text/javascript"></script>
+			<script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js" type="text/javascript"></script>
 		<![endif]-->
 
 		<!-- touch icons iOS 6 -->
