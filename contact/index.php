ce1639c0057a3187e54062b4983f595804e49991
Jesse Squires <jesse.d.squires@gmail.com>
2014-12-24 12:22:26 -0500

    fix bandcamp link

--- contact/index.php
+++ contact/index.php
@@ -18,7 +18,7 @@
 				<h3>Twitter</h3>
 				<p><a href="http://twitter.com/likexyoung" target="_blank">likexyoung</a></p><br />
 				<h3>Bandcamp</h3>
-				<p><a href="http://likexyoung.bandcamp.com" target="_blank">likexyoung</a></p><br />
+				<p><a href="https://likeyoungrecords.bandcamp.com" target="_blank">likeyoungrecords</a></p><br />
 				<h3>Tumblr</h3>
 				<p><a href="http://likeyoungrecords.tumblr.com" target="_blank">likeyoungrecords</a></p><br />
 				<h3>Last.fm</h3>
