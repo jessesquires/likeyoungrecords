151015a2f7158e7e5f7383ead1686432b74e0844
Jesse Squires <jesse.d.squires@gmail.com>
2015-04-05 11:41:18 -0700

    update samplers. add 2015 spring.

--- samplers/index.php
+++ samplers/index.php
@@ -12,20 +12,24 @@
 		<div id="samplers">
 			<h3>Samplers</h3>
 			<p>Here's a taste of each delightful year of Like Young Records &mdash; prepared just for you. 
-				If you hear something you like, head over to <a href="http://store.likeyoungrecords.com">the store</a> to support these amazing bands. 
-				Live like young.</p>
+				If you hear something you like, head over to <a href="http://store.likeyoungrecords.com">the store</a> to support these amazing bands, and live like young.</p>
 			<br />
 			<p class="tagline text-muted text-center">Listen. Share. Download. Enjoy.</p>
 			
 			<div class="row center col-md-10">
+            <h4 class="text-left">2015</h4>
+            <iframe style="border: 0; width: 100%; height: 180px;" src="http://bandcamp.com/EmbeddedPlayer/album=311730087/size=large/bgcol=333333/linkcol=ffffff/tracklist=false/artwork=small/transparent=true/" seamless>
+               <a href="http://likeyoungrecords.bandcamp.com/album/spring-sampler-2015">Spring Sampler 2015 by Like Young Records</a>
+            </iframe>
+
 				<h4 class="text-left">2014</h4>
 				<iframe style="border: 0; width: 100%; height: 180px;" src="http://bandcamp.com/EmbeddedPlayer/album=3668040003/size=large/bgcol=333333/linkcol=ffffff/tracklist=false/artwork=small/transparent=true/" seamless>
-					<a href="http://likexyoung.bandcamp.com/album/2014-sampler">2014 Sampler by Like Young</a>
+					<a href="http://likeyoungrecords.bandcamp.com/album/2014-sampler">2014 Sampler by Like Young</a>
 				</iframe>
 
 				<h4 class="text-left">2013</h4>
 				<iframe style="border: 0; width: 100%; height: 180px;" src="http://bandcamp.com/EmbeddedPlayer/album=1216911847/size=large/bgcol=333333/linkcol=ffffff/tracklist=false/artwork=small/transparent=true/" seamless>
-					<a href="http://likexyoung.bandcamp.com/album/like-young-records-sampler-2013">Like Young Records Sampler 2013. by Like Young</a>
+					<a href="http://likeyoungrecords.bandcamp.com/album/like-young-records-sampler-2013">Like Young Records Sampler 2013. by Like Young</a>
 				</iframe>
 
 			</div> <!-- row -->
