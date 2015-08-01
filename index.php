330649048e9ac64ce848b2735d668391abfb5dca
Jesse Squires <jesse.d.squires@gmail.com>
2015-08-01 10:51:30 -0700

    update to fb-page plugin

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
   <body>
     <?php include('includes/banner.php'); ?>
     <?php include('includes/menu.php'); ?>
-    
+
     <div class="row">
       <div id="about">
         <p>Like Young Records is a small, independent record label that is based in <a href="http://maps.google.com/maps/place?q=New+Albany+IN" target="_blank">New Albany, IN</a> (seriously, <em>that</em> place) and is run by <a href="http://www.facebook.com/jordan.reidinger" target="_blank">Jordan Reidinger</a>. Like Young primarily releases cassettes full of noise / ambient / minimal / indie / pop galore! And, we've even released hardcore and black metal. We are dedicated to spreading good music and helping bands as much as possible. Fuck money. Mind the <a href="http://en.wikipedia.org/wiki/Compact_Cassette" target="_blank">tape</a>.</p>
@@ -17,29 +17,33 @@
     </div> <!-- row -->
     <br />
     <div class="center" id="fb-container"></div>
-    
+
     <?php include('includes/footer.php'); ?>
 
     <div id="fb-root"></div>
     <script>
-      (function(d, s, id) {
-        var js, fjs = d.getElementsByTagName(s)[0];
-        if (d.getElementById(id)) return;
-        js = d.createElement(s); js.id = id;
-        js.src = "//connect.facebook.net/en_US/all.js#xfbml=1";
-        fjs.parentNode.insertBefore(js, fjs);
-      }(document, 'script', 'facebook-jssdk'));
+    (function(d, s, id) {
+      var js, fjs = d.getElementsByTagName(s)[0];
+      if (d.getElementById(id)) return;
+      js = d.createElement(s); js.id = id;
+      js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.4";
+      fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));
 
-      $(window).bind("load resize", function() {   
-        var container_width = $('#banner').width();    
-        
-        $('#fb-container').html('<div class="fb-like-box" ' + 
-        'data-href="https://www.facebook.com/likexyoung"' +
-        ' data-width="' + container_width + 
-        '" data-height="500" data-colorscheme="dark" data-show-faces="false" ' +
-        'data-stream="true" data-show-border="false" data-header="false"></div>');
-        FB.XFBML.parse( );    
-    }); 
+    $(window).bind("load resize", function() {
+      var container_width = $('#banner').width();
+      $('#fb-container').html('<div class="fb-page" ' +
+      'data-href="https://www.facebook.com/likexyoung" ' +
+      'data-width="' + container_width + '" ' +
+      'data-height="500" ' +
+      'data-small-header="false" ' +
+      'data-adapt-container-width="true" ' +
+      'data-hide-cover="false" ' +
+      'data-show-facepile="false" ' +
+      'data-show-posts="true">' +
+      '</div>');
+      FB.XFBML.parse();
+    });
     </script>
 
   </body>
