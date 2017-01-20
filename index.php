2965b05c03ccbe0a3c48ede6ddb8d524104a86ce
Jesse Squires <jesse.d.squires@gmail.com>
2017-01-19 17:34:04 -0800

    fuck donald trump. #fdt

--- index.php
+++ index.php
@@ -12,7 +12,8 @@
     <div class="row">
       <div id="about">
         <p>Like Young Records is a small, independent record label that is based in <a href="http://maps.google.com/maps/place?q=New+Albany+IN" target="_blank">New Albany, IN</a> (seriously, <em>that</em> place) and is run by <a href="http://www.facebook.com/jordan.reidinger" target="_blank">Jordan Reidinger</a>. Like Young primarily releases cassettes full of noise / ambient / minimal / indie / pop galore! And, we've even released hardcore and black metal. We are dedicated to spreading good music and helping bands as much as possible. Fuck money. Mind the <a href="http://en.wikipedia.org/wiki/Compact_Cassette" target="_blank">tape</a>.</p>
-        <p class="tagline text-muted text-center">Tonight, we will live like young.<br />Welcome to our cult.</p>
+        <p class="tagline text-muted text-center">
+        <a href="http://store.likeyoungrecords.com/product/3-button-set">Fuck Donald Trump.</a><br/>Tonight, we will live like young.<br />Welcome to our cult.</p>
       </div> <!-- about -->
     </div> <!-- row -->
     <br />
