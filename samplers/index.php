f35951a879f90bb66768d7f3c5c8a6188014bda6
Jesse Squires <jesse@jessesquires.com>
2017-07-30 13:09:04 -0700

    fix up samples page

--- samplers/index.php
+++ samplers/index.php
@@ -18,23 +18,27 @@
 
 			<div class="row center col-md-10">
                 <h4 class="text-left">2017</h4>
-                <iframe style="border: 0; width: 100%; height: 180px;" src="http://bandcamp.com/EmbeddedPlayer/album=1609032322/size=large/bgcol=333333/linkcol=ffffff/tracklist=false/artwork=small/transparent=true/" seamless>
+                <p><a href="https://likeyoungrecords.bandcamp.com/album/spring-sampler-2017">Spring Sampler 2017 by Like Young Records</a></p>
+                <iframe style="border: 0; width: 100%; height: 180px;" src="https://bandcamp.com/EmbeddedPlayer/album=1609032322/size=large/bgcol=333333/linkcol=ffffff/tracklist=false/artwork=small/transparent=true/" seamless>
                    <a href="https://likeyoungrecords.bandcamp.com/album/spring-sampler-2017">Spring Sampler 2017 by Like Young Records</a>
                 </iframe>
 
                 <h4 class="text-left">2015</h4>
-                <iframe style="border: 0; width: 100%; height: 180px;" src="http://bandcamp.com/EmbeddedPlayer/album=311730087/size=large/bgcol=333333/linkcol=ffffff/tracklist=false/artwork=small/transparent=true/" seamless>
-                   <a href="http://likeyoungrecords.bandcamp.com/album/spring-sampler-2015">Spring Sampler 2015 by Like Young Records</a>
+                <p><a href="https://likeyoungrecords.bandcamp.com/album/spring-sampler-2015">Spring Sampler 2015 by Like Young Records</a></p>
+                <iframe style="border: 0; width: 100%; height: 180px;" src="https://bandcamp.com/EmbeddedPlayer/album=311730087/size=large/bgcol=333333/linkcol=ffffff/tracklist=false/artwork=small/transparent=true/" seamless>
+                   <a href="https://likeyoungrecords.bandcamp.com/album/spring-sampler-2015">Spring Sampler 2015 by Like Young Records</a>
                 </iframe>
 
                 <h4 class="text-left">2014</h4>
-                <iframe style="border: 0; width: 100%; height: 180px;" src="http://bandcamp.com/EmbeddedPlayer/album=3668040003/size=large/bgcol=333333/linkcol=ffffff/tracklist=false/artwork=small/transparent=true/" seamless>
-                    <a href="http://likeyoungrecords.bandcamp.com/album/2014-sampler">2014 Sampler by Like Young</a>
+                <p><a href="https://likeyoungrecords.bandcamp.com/album/2014-sampler">2014 Sampler by Like Young</a></p>
+                <iframe style="border: 0; width: 100%; height: 180px;" src="https://bandcamp.com/EmbeddedPlayer/album=3668040003/size=large/bgcol=333333/linkcol=ffffff/tracklist=false/artwork=small/transparent=true/" seamless>
+                    <a href="https://likeyoungrecords.bandcamp.com/album/2014-sampler">2014 Sampler by Like Young</a>
                 </iframe>
 
                 <h4 class="text-left">2013</h4>
-                <iframe style="border: 0; width: 100%; height: 180px;" src="http://bandcamp.com/EmbeddedPlayer/album=1216911847/size=large/bgcol=333333/linkcol=ffffff/tracklist=false/artwork=small/transparent=true/" seamless>
-                    <a href="http://likeyoungrecords.bandcamp.com/album/like-young-records-sampler-2013">Like Young Records Sampler 2013. by Like Young</a>
+                <p><a href="https://likeyoungrecords.bandcamp.com/album/like-young-records-sampler-2013">Like Young Records Sampler 2013. by Like Young</a></p>
+                <iframe style="border: 0; width: 100%; height: 180px;" src="https://bandcamp.com/EmbeddedPlayer/album=1216911847/size=large/bgcol=333333/linkcol=ffffff/tracklist=false/artwork=small/transparent=true/" seamless>
+                    <a href="https://likeyoungrecords.bandcamp.com/album/like-young-records-sampler-2013">Like Young Records Sampler 2013. by Like Young</a>
                 </iframe>
 
 			</div> <!-- row -->
